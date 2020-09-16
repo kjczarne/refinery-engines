73a095703ae1912e2dda885b16a0f14f05136cf9
Krzysztof Czarnecki <kjczarne@Krzysztofs-MBP.home>
2020-09-17 00:29:49 +0200

    initial commit, ported from core refinery package

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare module 'refinery-engines';
+
+export { BaseEngine } from './built/engines/baseEngine';
+export { MdEngine } from './built/engines/markdown';
+export { AppleiBooksEngine } from './built/engines/iBooks';
+export { JsonEngine } from './built/engines/json';
+export { AndevFldsEngine } from './built/engines/andevFlashcards';
---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var baseEngine_1 = require("./built/engines/baseEngine");
+Object.defineProperty(exports, "BaseEngine", { enumerable: true, get: function () { return baseEngine_1.BaseEngine; } });
+var markdown_1 = require("./built/engines/markdown");
+Object.defineProperty(exports, "MdEngine", { enumerable: true, get: function () { return markdown_1.MdEngine; } });
+var iBooks_1 = require("./built/engines/iBooks");
+Object.defineProperty(exports, "AppleiBooksEngine", { enumerable: true, get: function () { return iBooks_1.AppleiBooksEngine; } });
+var json_1 = require("./built/engines/json");
+Object.defineProperty(exports, "JsonEngine", { enumerable: true, get: function () { return json_1.JsonEngine; } });
+var andevFlashcards_1 = require("./built/engines/andevFlashcards");
+Object.defineProperty(exports, "AndevFldsEngine", { enumerable: true, get: function () { return andevFlashcards_1.AndevFldsEngine; } });
+//# sourceMappingURL=index.js.map
